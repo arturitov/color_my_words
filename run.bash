9e63b1d9bb2755d7369ef560d3c84e215fcb2320
Arturo Valery <aav13@my.fsu.edu>
2016-05-01 14:29:25 -0400

    Added lyrics to run file

--- run.bash
+++ run.bash
@@ -3,7 +3,8 @@
 while true
 do
 	python alchemyapi.py 1965c860662b3ec66ee989b20323f5473e9fa89b
-	python scrape_article.py | tee txt/new.txt
+	python scrape_article.py news | tee txt/new.txt
+	python scrape_article.py lyrics | tee txt/lycs.txt
 	cat txt/new.txt >> txt/log.txt
 	cat txt/new.txt
 	echo "Sleeping..."
